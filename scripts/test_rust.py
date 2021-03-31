32a96c1bcc01310b569d56ddd5c26d10298938ec
Brian Merchant <bhmerchant@gmail.com>
2021-03-31 14:36:58 -0700

    checkpoint

--- scripts/test_rust.py
+++ scripts/test_rust.py
@@ -10,7 +10,7 @@ import orjson
 run_experiments = True
 exec_mode = "release"
 root_dir = os.getcwd()
-exp_jsons = ["four_cell_adh_5_cal_60"]
+exp_jsons = ["nine_cell_adh_180"]
 for exp_json in exp_jsons:
     exec_path = os.path.join(root_dir, "target", exec_mode, "executor")
     if run_experiments:
