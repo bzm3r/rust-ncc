5710d94a2007da315ddbe2019e101ad43858a7f4
Brian Merchant <bhmerchant@gmail.com>
2021-04-29 13:47:06 -0700

    license updates

--- executor/src/main.rs
+++ executor/src/main.rs
@@ -1,3 +1,11 @@
+// Copyright © 2020 Brian Merchant.
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
 use clap::{App, AppSettings, Arg};
 use simulator::exp_setup::exp_parser::ExperimentArgs;
 use simulator::{exp_setup, world, Directories};
